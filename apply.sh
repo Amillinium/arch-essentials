6fdecf3d9d30ef86c01b75bafe095edf7e206604
Amillinium <cr.esmaeeili@yahoo.com>
2024-06-02 17:55:36 +0330

    Update apply.sh

--- apply.sh
+++ apply.sh
@@ -1,7 +1,7 @@
 sudo cp local.conf /etc/fonts/local.conf
-sudo cp pacman.conf /etc/pacman.conf
 sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+sudo cp pacman.conf /etc/pacman.conf
 echo "Arch essentials applied successfully"
