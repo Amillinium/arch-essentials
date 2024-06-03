43a7f0e49868d138b23e3613588f46d4d030d38d
Amillinium <cr.esmaeeili@yahoo.com>
2024-06-03 12:29:00 +0330

    Add files via upload

--- apply.sh
+++ apply.sh
@@ -1,7 +1,8 @@
 sudo cp local.conf /etc/fonts/local.conf
+sudo cp pacman.conf /etc/pacman.conf
+sudo cp mirrorlist /etc/pacman.d/mirrorlist
 sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key 3056513887B78AEB
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
 sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-sudo cp pacman.conf /etc/pacman.conf
 echo "Arch essentials applied successfully"
